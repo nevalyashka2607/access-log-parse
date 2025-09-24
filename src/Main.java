45d5eb92d051353440854b6d23dab5d1321b4d52
OVMoiseev <nevalyashka2607moiseev@yandex.ru>
2025-09-24 12:03:22 +0300

    Исправление опечаток

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.print("Введите первое число: ");
-        long fitstnumber = new Scanner(System.in).nextLong();
+        long firstNumber = new Scanner(System.in).nextLong();
         System.out.print("Введите второе число: ");
-        long secondnumber = new Scanner(System.in).nextLong();
-
-        long sum = fitstnumber + secondnumber;
+        long secondNumber = new Scanner(System.in).nextLong();
+        long sum = firstNumber + secondNumber;
         System.out.println("Сумма чисел: " + sum);
-
-        long a = fitstnumber;
-        long b = secondnumber;
-
-        System.out.println("Разность чисел: " + (a - b)); ///Можно поставить  Math.abs чтобы не появлялось отриццательного числа
-
-        long a1 = fitstnumber;
-        long a2 = secondnumber;
-        System.out.println("Произведение чисел : " + (a1 * a2));
-
-        long a4 = fitstnumber;
-        long a5 = secondnumber;
-        System.out.println("Частное чисел :" + ((double) a4 / a5));
+        System.out.println("Разность чисел: " + (firstNumber - secondNumber)); ///Можно поставить  Math.abs чтобы не появлялось отриццательного числа
+        System.out.println("Произведение чисел : " + (firstNumber * secondNumber));
+        System.out.println("Частное чисел :" + ((double) firstNumber / secondNumber));
 
 
     }
